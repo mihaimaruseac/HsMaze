6fbb90521f1cfccbd9dca80b09531d493713ff1e
Mihai Maruseac <mihai.maruseac@rosedu.org>
2011-05-11 19:17:01 +0300

    New types

--- Types.hs
+++ Types.hs
@@ -9,18 +9,29 @@ type Point = Size
 type Dir = Size
 
 {- The cardinal directions. -}
-data Cardinal = N | E | S | W
-  deriving (Eq, Show, Read, Ord, Enum)
+data Cardinal = N | E | S | W deriving (Eq, Show, Read, Ord, Enum)
 
 {- A cell. The list contains the openings. -}
-newtype Cell = C [Cardinal]
+newtype Cell = C [Cardinal] deriving (Show, Read)
+
+{-
+A plan to leave the maze. If the plane ends and the time is not spent, the
+plan is restarted. This allows for simple plans while having complicated
+mazes.
+-}
+data Plan
+  = Go Cardinal -- go in one direction
+  | Seq Plan Plan -- do one plan, then another
+  | If Cond Plan Plan -- do one plan or another
   deriving (Show, Read)
 
-{- Block a cell from one direction. -}
-block :: Cell -> Cardinal -> Cell
-block (C l) x = C $ filter (/= x) l
-
-{- Open a cell to one direction. -}
-open :: Cell -> Cardinal -> Cell
-open (C l) x = C $ if x `elem` l then l else x : l
+{-
+A condition to use in a plan. The only input is the neighborhood.
+-}
+data Cond
+  = Free Cardinal -- cell in that direction is reachable
+  | Blocked Cardinal -- way blocked
+  | Or Cond Cond -- one condition or another
+  | And Cond Cond -- both conditions
+  deriving (Show, Read)
 
